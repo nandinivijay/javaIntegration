1af9900f65966d20edcf473b940cde5215588a61
suhasini41 <suhasini.mb@gmail.com>
2021-09-28 10:24:40 -0500

    committed

--- src/javaGitIntegration/Calculator.java
+++ src/javaGitIntegration/Calculator.java
@@ -2,15 +2,23 @@ package javaGitIntegration;
 
 public class Calculator {
 
-	public Calculator() {
-		// TODO Auto-generated constructor stub
+	public Calculator() 
+	{
+		
 	}
 	
-	
+	public static void division()
+	{
+		int qoutient = 0;
+		int x=12,y=3;
+		qoutient = x/y;
+		System.out.println("Quotient is : "+qoutient);
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 	}
+	
 
 }
