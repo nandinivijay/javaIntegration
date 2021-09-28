0cbd162cecb3d50a78dca9b36168d15f1f007120
Nandini Rajaram <nandinirajaram@gmail.com>
2021-09-28 11:00:00 -0500

    TA- subtraction

--- src/javaGitIntegration/Calculator.java
+++ src/javaGitIntegration/Calculator.java
@@ -10,7 +10,7 @@ public class Calculator {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+System.out.println(subtraction(10,20));
 	}
 	
 	public static int subtraction(int a,int b) {
