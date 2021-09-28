bf53cc0496d611d34a8fc10cf04c653c40ea7bcd
AkilaBV <akilabv@gmail.com>
2021-09-28 10:20:17 -0500

    add is committed

--- src/javaGitIntegration/Calculator.java
+++ src/javaGitIntegration/Calculator.java
@@ -1,15 +1,19 @@
 package javaGitIntegration;
 
 public class Calculator {
-
+	static int  a;
 	public Calculator() {
-		// TODO Auto-generated constructor stub
+		
+		
 	}
 	
-	
+	public static int add(int b,int c) {
+		a=b+c;
+		return a;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("sum is : " +add(5,10));
 
 	}
 
