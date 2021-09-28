1e8366327932163aa6c1361fc3fb08d80f1128ea
kavitha jaghni <kavitha.jaghni@gmail.com>
2021-09-28 11:22:19 -0400

    multiplication updated

--- src/javaGitIntegration/Calculator.java
+++ src/javaGitIntegration/Calculator.java
@@ -2,15 +2,22 @@ package javaGitIntegration;
 
 public class Calculator {
 
-	public Calculator() {
+	public void Calculator() {
+	}
+		
 		// TODO Auto-generated constructor stub
+	public static int multiply(int a,int b)
+	{
+		int mul=a*b;
+		return mul;
+		
 	}
 	
 	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+  System.out.println(multiply(3,6));
 	}
 
 }
